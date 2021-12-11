272bbff94f83f2698436cdbd7e7f919ef2de3267
Bahtiyar <bahtiyarmceylan@gmail.com>
2021-12-11 02:47:18 -0500

    update version no of json files

--- js/classes-data.js
+++ js/classes-data.js
@@ -17,7 +17,7 @@ class Champion {
 	constructor() { }
 
 	static loadChampions(callback) {
-		$.ajax({type: "GET", url: "data.json", dataType: "json",
+		$.ajax({type: "GET", url: "data.json?v=0.2.32", dataType: "json",
 			success: (data, textStatus) => callback(data),
 			error: (textStatus, errorThrown) => console.error(errorThrown)
 		});
@@ -91,7 +91,7 @@ class Tags {
 	constructor() { }
 
 	static loadTags(callback) {
-		$.ajax({type: "GET", url: "data-tags.json", dataType: "json",
+		$.ajax({type: "GET", url: "data-tags.json?v=0.2.32", dataType: "json",
 			success: (data, textStatus) => callback(data),
 			error: (textStatus, errorThrown) => console.error(errorThrown)
 		});
