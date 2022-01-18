9abbab8b6d7fa9690099fb5b80c32da546a2c8ca
Bahtiyar Ceylan <50676218+bahtyr@users.noreply.github.com>
2022-01-18 16:34:48 -0500

    show current patch info, new hoverable tooltip class

--- js/index/index.js
+++ js/index/index.js
@@ -1,6 +1,7 @@
 var champions = [];
 var tags      = [];
 var menu      = [];
+var patches   = {};
 var champlist = new ChampListManager;
 var champcard = new ChampCardManager;
 var sidebar   = new SidebarManager;
@@ -23,6 +24,11 @@ fetch("data/tags.json").then(data => data.json()).then(json => {
 	matchChampionsAndTags();
 });
 
+fetch("data/patches.json").then(data => data.json()).then(json => {
+	patches = json;
+	printPatchInfo();
+});
+
 /* wait for both champions & tags to be initiated, then run only once */
 let champTagsMatched = false;
 function matchChampionsAndTags() {
@@ -37,6 +43,27 @@ function matchChampionsAndTags() {
 			if (t.includes(tag.id)) tag.champIndexes.push(index);
 		});
 	}));
+}
+
+/* find current patch and print */
+function printPatchInfo() {
+	/**
+	 * 1. start from the last and check if the date is bigger than today
+	 * 2. current + 1 is next patch, unless it is the last known patch
+	 * 3. if next == current, we reached to the end of known pacthes, don't show next patch date
+	 */
+	let today = Date.now();
+	let current = patches.length - 1;
+	while (patches[current].start * 1000 > today) current--;
+
+	let next = current == patches.length ? current : current + 1;
+
+	let nextPatchDate = new Date(patches[next].start * 1000).toLocaleDateString("en-US", {month: "short", weekday: "short", day: "numeric"});
+	let tooltip = $class("patch-version-tooltip")[0];
+	$id("patch-version").textContent = "V" + patches[current].version;
+	tooltip.children[0].textContent = "V" + patches[current].version;
+	tooltip.children[1].textContent = "Next Patch: " + (next == current ? "N/A" : nextPatchDate);
+	tooltip.children[2].setAttribute("href", patches[current].link);
 }
 
 /*************************************************************************************************/
