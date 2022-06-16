ad5f3890cd1562d2d23cdd409b02cb76941d35af
Bahtiyar Ceylan <50676218+bahtyr@users.noreply.github.com>
2022-06-16 14:49:42 -0400

    edit: read/save champ data changes #34

--- js/edit/edit-ui.js
+++ js/edit/edit-ui.js
@@ -137,6 +137,104 @@ class EditUiManager {
 		$id("ability-4-video").value = champions[i].abilities[4].video;
 	}
 
+	/****************************************** READ *********************************************/
+
+	readChampData() {
+		let form = $id("champion-form");
+		let i = this.selectedChampIndex;
+		// let champ = {ids: {}, abilities: [{},{},{},{},{}], ratings: {damageBreakdown: {}}};
+
+		champions[i].ids.kebab = $id("idKebab").value;
+		champions[i].ids.ddragon = $id("idDdragon").value;
+		champions[i].ids.cdragon = $id("idCdragon").value;
+		champions[i].ids.wiki = $id("idWiki").value;
+		champions[i].ids.universe = $id("idUniverse").value;
+		
+		champions[i].portrait = $id("img-portrait").src;
+		champions[i].name = $id("name").value;
+		champions[i].title = $id("title").value;
+		champions[i].portrait = $id("portrait").value;
+
+		champions[i].lanes = "";
+		if ($id("laneTop").checked) champions[i].lanes += "Top ";
+		if ($id("laneJg").checked)  champions[i].lanes += "Jungle ";
+		if ($id("laneMid").checked) champions[i].lanes += "Middle ";
+		if ($id("laneBot").checked) champions[i].lanes += "Bottom ";
+		if ($id("laneSup").checked) champions[i].lanes += "Support ";
+		champions[i].lanes = champions[i].lanes.trim();
+
+		champions[i].tags = [];
+		if ($id("roleFighter").checked)  champions[i].tags.push("Fighter");
+		if ($id("roleBuiser").checked)   champions[i].tags.push("Bruiser");
+		if ($id("roleAssassin").checked) champions[i].tags.push("Assassin");
+		if ($id("roleMage").checked)     champions[i].tags.push("Mage");
+		if ($id("roleMarksman").checked) champions[i].tags.push("Marksman");
+		if ($id("roleSupport").checked)  champions[i].tags.push("Support");
+
+		champions[i].resource = $id("resource").value;
+		champions[i].attackRange = $id("attackRange").value;
+		if ($id("rangeTypeRanged").checked) champions[i].rangeType = "Ranged";
+		if ($id("rangeTypeMelee").checked)  champions[i].rangeType = "Melee";
+
+		champions[i].ratings.damage = $id("ratingDamage").value;
+		champions[i].ratings.mobility = $id("ratingMobility").value;
+		champions[i].ratings.toughness = $id("ratingToughness").value;
+		champions[i].ratings.control = $id("ratingControl").value;
+		champions[i].ratings.utility = $id("ratingUtility").value;
+
+		champions[i].ratings.difficulty = $id("difficulty").value;
+		champions[i].ratings.style = $id("style").value;
+		if (champions[i].ratings.damageBreakdown == null)
+			champions[i].ratings.damageBreakdown = {};
+		champions[i].ratings.damageBreakdown.magic = $id("dmgMagic").value;
+		champions[i].ratings.damageBreakdown.physical = $id("dmgPhysical").value;
+		champions[i].ratings.damageBreakdown.true_ = $id("dmgTrue_").value;
+
+		champions[i].releaseDate = $id("releaseDate").value;
+		champions[i].releasePatch = $id("releasePatch").value;
+
+		champions[i].region = $id("region").value;
+		champions[i].species = $id("species").value;
+
+		champions[i].spotlightVideoID = $id("spotlightVideoID").value;
+
+		champions[i].abilities[0].img = $id("img-ability-0").src;
+		champions[i].abilities[0].name = $id("ability-0-name").value;
+		champions[i].abilities[0].description = $id("ability-0-description").value;
+		champions[i].abilities[0].img = $id("ability-0-img").value;
+		champions[i].abilities[0].video = $id("ability-0-video").value;
+		
+		champions[i].abilities[1].img = $id("img-ability-1").src;
+		champions[i].abilities[1].name = $id("ability-1-name").value;
+		champions[i].abilities[1].description = $id("ability-1-description").value;
+		champions[i].abilities[1].img = $id("ability-1-img").value;
+		champions[i].abilities[1].video = $id("ability-1-video").value;
+		
+		champions[i].abilities[2].img = $id("img-ability-2").src;
+		champions[i].abilities[2].name = $id("ability-2-name").value;
+		champions[i].abilities[2].description = $id("ability-2-description").value;
+		champions[i].abilities[2].img = $id("ability-2-img").value;
+		champions[i].abilities[2].video = $id("ability-2-video").value;
+		
+		champions[i].abilities[3].img = $id("img-ability-3").src;
+		champions[i].abilities[3].name = $id("ability-3-name").value;
+		champions[i].abilities[3].description = $id("ability-3-description").value;
+		champions[i].abilities[3].img = $id("ability-3-img").value;
+		champions[i].abilities[3].video = $id("ability-3-video").value;
+
+		champions[i].abilities[4].img = $id("img-ability-4").src;
+		champions[i].abilities[4].name = $id("ability-4-name").value;
+		champions[i].abilities[4].description = $id("ability-4-description").value;
+		champions[i].abilities[4].img = $id("ability-4-img").value;
+		champions[i].abilities[4].video = $id("ability-4-video").value;
+
+		if (champions[i].tagArrays == null)
+			champions[i].tagArrays = [[],[],[],[],[],[]];
+
+		log(`Saved ${i}:${champions[i].name}.`);
+	}
+
+
 	/****************************************** ONCLICK ******************************************/
 
 	onClickFunc(i) {
@@ -144,6 +242,7 @@ class EditUiManager {
 	}
 
 	onClickChamp(i) {
+		pageManager.selectedChampIndex = i;
 		pageManager.populateChampData(i);
 	}
 
